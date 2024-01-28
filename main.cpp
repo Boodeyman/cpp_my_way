79e8a48934e4d023d27f8cbc5a0587be8572efab
Boodeyman <bogdafan31@gmail.com>
2024-01-28 17:40:47 +0300

    1st ex

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//    cout << "Hello, World!";
-//    return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
 
 
 
