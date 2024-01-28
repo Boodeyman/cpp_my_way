28091854f28ae9c5b21dc5915c46e09b91266421
Boodeyman <bogdafan31@gmail.com>
2024-01-28 17:36:43 +0300

    1st ex

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    cout << "Hello, World!";
+//    return 0;
+//}
 
-using namespace std;
 
-int main() {
-    cout << "Hello, World!";
-    return 0;
-}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int n_1, n_2, n_3, res;
+//
+//    cin >> n_1;
+//    cin >> n_2;
+//    cin >> n_3;
+//    res = n_1 * n_2 + n_3;
+//    cout << n_1 << " * " << n_2 << " + " << n_3 << " = " << res << endl;
+//}
+
+
