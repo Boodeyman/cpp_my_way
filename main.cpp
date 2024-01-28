cd986db5b1f53e8f6b7853dc68edadd6341c951b
Boodeyman <bogdafan31@gmail.com>
2024-01-28 22:18:00 +0300

    contest push

--- main.cpp
+++ main.cpp
@@ -22,3 +22,24 @@
 //    res = n_1 * n_2 + n_3;
 //    cout << n_1 << " * " << n_2 << " + " << n_3 << " = " << res << endl;
 //}
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int cord_x1, cord_y1, cord_x2, cord_y2;
+    cin >> cord_x1;
+    cin >> cord_y1;
+    cin >> cord_x2;
+    cin >> cord_y2;
+    if ((cord_x1 == cord_x2) || (cord_y1 == cord_y2)) {
+        cout << "YES";
+    } else if ((cord_x1 == cord_y1) && (cord_y2 == cord_x2)) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
